fdefb8ad6a878555e6fa89340fb4ff9bef762b7e
cristianpb <felipebrokate@gmail.com>
2018-11-18 22:46:36 +0100

    Send image and url for news handler

--- handler.js
+++ handler.js
@@ -8,14 +8,20 @@ const handler = new MessengerHandler()
   .onText(/news/i, async context => {
     const connection = await MongoClient.connect(config.mongourl, { useNewUrlParser: true });
     const db = await connection.db('newsapp');
-    const res1 = await db.collection('news')
-      .find({})
-      .toArray();
-    if (!process.env.USE_CONSOLE) {
-      const attachment = getAttachment(res1[0]['urlToImage']);
-      await context.sendImage({ attachment_id: attachment.id });
+    try {
+      const res1 = await db.collection('news')
+        .find({})
+        .toArray();
+      await context.sendText(`From: ${res1[0]['source']['name']}, \n${res1[0]['title']} \nPublished at: ${res1[0]['publishedAt']}\n${res1[0]['url']}`);
+      if (!process.env.USE_CONSOLE) {
+        await context.sendImage(res1[0]['urlToImage']);
+      }
+    } catch (e) {
+      console.log(e);
     }
-    await context.sendText(res1[0]['title']);
+  })
+  .onText(/yo/i, async context => {
+    await context.sendText('Hello there');
   })
   .onEvent(async context => {
     await context.sendText("I don't know what you say.");
